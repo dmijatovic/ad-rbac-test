67e51a8549e88aa390cd8e50b748e4f2328f9f85
Dusan Mijatovic (PC2016) <d.mijatovic@dv4all.nl>
2020-10-04 14:33:15 +0200

    feat: added more data to dummt article

--- ad-node-api/src/db/article.js
+++ ad-node-api/src/db/article.js
@@ -4,13 +4,47 @@ const article={
   aid:"",
   title:"This is article title",
   url:"https://nu.nl",
-  author:"Author name"
+  author:"Author name",
+  newsgroups:[
+    "ING Domestic Radio & TV",
+    "ING Domestic",
+    "ING Group",
+    "ING Voetbal"
+  ],
+  medium:"banken.nl",
+  url:"https://www.banken.nl/nieuws/22650/rabobank-stelt-negatieve-rente-in-voor-spaartegoeden-boven-250000",
+  page: 6,
+  articleDate:"2020-09-21",
+  circulation:"This is circulation comment",
+  modules:[
+    {
+      id:"1", name:"Competitors",
+      variables:[{
+        id:1,label:"Competitor", type:"select",
+        options:["Option 1","Option 2"]
+      },{
+        id:2,label:"First mention", type:"number",
+      },{
+        id:3,label:"Comment", type:"text"
+      }]
+    },{
+      id:"2", name:"Module 2",
+      variables:[{
+        id:1,label:"Competitor", type:"select",
+        options:["Option 1","Option 2"]
+      },{
+        id:2,label:"First mention", type:"number",
+      },{
+        id:3,label:"Comment", type:"text"
+      }]
+    }
+  ]
 }
 
 
 function getArticle(cid,aid){
   article.cid = cid
-  article.aid = aid 
+  article.aid = aid
 
   return article
 }
